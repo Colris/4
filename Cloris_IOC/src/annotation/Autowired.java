09578a1efce15f500986becbd68758ff8625522e
Cloris <14301124@bjtu.edu.cn>
2016-11-12 19:03:21 +0800

    5

---
+++ Cloris_IOC/src/annotation/Autowired.java
@@ -0,0 +1,15 @@
+package annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+
+/**
+ * 
+ * @author Cloris
+ *
+ */
+@Retention(RUNTIME)
+public @interface Autowired {
+
+}
---
+++ Cloris_IOC/src/myIOC/ApplicationContext.java
@@ -0,0 +1,12 @@
+package myIOC;
+
+/**
+ * 
+ * @author Cloris
+ *
+ */
+public interface ApplicationContext {
+	
+	public Object getBean(String bean);
+
+}
